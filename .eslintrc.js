afab75a499673def350d0d0058e7559c0f9a5bd2
EvanTsai <dakr1988128@gmail.com>
2020-08-28 23:03:48 +0800

    Add : eslint + prettier環境 + scss

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    "plugin:vue/essential", // 使用 essential 規範
+    "eslint:recommended", // 使用 ESLint 推薦規範
+    "@vue/prettier", //使用 prettier
+  ],
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: "module",
+  },
+};
